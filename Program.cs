9bf0a983eea50555d34e81174f40842316c0b87d
Dennis <den.peld@gmail.com>
2019-10-27 15:01:53 +0200

    MarsRover: final solution

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MarsRover
+{
+    internal static class Program
+    {
+        private const string Input = @"5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM";
+
+        private static void Main()
+        {
+            var reader = new InputReader(Input);
+
+            Console.ReadLine();
+        }
+    }
+}
